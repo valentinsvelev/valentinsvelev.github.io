b04dfd5fb5df4026095b79424b0f53d5ac55201a
valentin-velev29 <valentin.velev@gmx.de>
2024-09-01 23:33:52 +0200

    more style changes + folder for cv

--- scripts.js
+++ scripts.js
@@ -1,30 +1,12 @@
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+// Highlight current website
+document.addEventListener("DOMContentLoaded", function() {
+    const currentLocation = window.location.href;
+    const navLinks = document.querySelectorAll("nav ul li a");
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    navLinks.forEach((link) => {
+        if (link.href === currentLocation) {
+            link.classList.add("active");
+        }
     });
 });
-
-// Function to adjust font sizes based on window width
-function adjustFontSize() {
-    const h1 = document.querySelector('h1');
-    const windowWidth = window.innerWidth;
-
-    if (windowWidth <= 600) {
-        h1.style.fontSize = '18px'; // Mobile size
-    } else if (windowWidth > 600 && windowWidth <= 768) {
-        h1.style.fontSize = '20px'; // Tablet size
-    } else {
-        h1.style.fontSize = '30px'; // Desktop size
-    }
-}
-
-// Run the function on page load
-window.addEventListener('load', adjustFontSize);
-
-// Run the function on window resize
-window.addEventListener('resize', adjustFontSize);
