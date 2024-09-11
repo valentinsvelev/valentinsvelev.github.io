73a64c6dd406bd586a06db64861469ec814171ea
valentin-velev29 <valentin.velev@gmx.de>
2024-09-11 16:18:15 +0200

    more adjustments

--- scripts.js
+++ scripts.js
@@ -29,3 +29,37 @@ document.querySelectorAll(".downloadBtn").forEach(function(button) {
         document.body.removeChild(link);
     });
 });
+
+// Responsiveness
+window.addEventListener('resize', function() {
+    var width = window.innerWidth;
+    var headerContainer = document.querySelector('.header-container');
+    var navToggle = document.querySelector('.nav-toggle');
+    var container = document.querySelector('.header-info');
+    var container_h1 = container.querySelector('h1');
+    var container_p = container.querySelector('p');
+    var titleText = document.querySelector('.title-text');
+    var footer = document.querySelector('.footer');
+
+    // Adjust font sizes and center header text
+    if (width < 768) {
+        headerContainer.style.textAlign = 'center';
+        container_h1.style.fontSize = '30px';
+        container_p.style.fontSize = '18px';
+        titleText.style.fontSize = '22px';
+        footer.style.fontSize = '15px';
+    }
+
+    // Center hamburger button
+    navToggle.style.margin = 'auto';
+});
+
+// Select the hamburger button and the nav element
+var navToggle = document.querySelector('.nav-toggle');
+var headerNav = document.querySelector('.header-nav');
+
+// Add a click event listener to the hamburger button
+navToggle.addEventListener('click', function() {
+    // Toggle the "active" class on the header nav element
+    headerNav.classList.toggle('active');
+});
